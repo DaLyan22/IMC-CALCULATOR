e32988d5f226630ae725bbb0ca47cfa2e700a773
Gabriel Soares <123920104+DaLyan22@users.noreply.github.com>
2023-04-10 23:35:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
 const form = document.getElementById(
-
- 'form')
-
-form.addEventListener('submit',
-
- enviar);
-
-function enviar(e) {
-
- e.preventDefault();
-
- const peso = document.getElementById(
-
-  'peso')
-
- const altura = document
-
-  .getElementById('altura')
-
- const imc = document.getElementById(
-
-  'imc')
-
- imc.innerHTML = "Seu IMC é: " + (peso
-
-  .value / altura.value / altura
-
-  .value)
-
-}
-
+    'form')
+   
+   form.addEventListener('submit',
+    enviar);
+   
+   
+   
+   function enviar(e) {
+    e.preventDefault();
+    const peso = document.getElementById(
+     'peso')
+    const altura = document.getElementById('altura')
+   
+    const imc = document.getElementById('imc')
+   
+    imc.innerHTML = "Seu IMC é: " + Math.round(peso
+     .value / altura.value / altura
+     .value * 10000)
+   
+   }
